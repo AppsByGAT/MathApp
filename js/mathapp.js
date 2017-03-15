abfde1a35ea0af03cd0e05247a294b7308e65f93
Gregory Tewksbury <GregATewksbury@gmail.com>
2017-03-15 12:22:13 -0400

    first commit

---
+++ js/mathapp.js
@@ -0,0 +1,5 @@
+// JavaScript BootStrap Tooltip Display
+// Code from www.w3schools.com/bootstrap/bootstrap_tooltip.asp
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
